f12fb9b71770a4db8db089bcd442cc0646868461
Aman Singh <aman.it360@gmail.com>
2024-10-22 21:14:56 +0530

    Updated Project Files

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,4 +1,176 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator");
+const bcrypt = require("bcrypt");
+
 //sendOTP
+exports.sendOTP = async (req, res) => {
+  try {
+    //fetch email from req body
+    const { email } = req.body;
+
+    //check if user is already exist
+    const checkUserPresent = await User.findOne({ email });
+
+    //if user already exist , then return a response
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: "User already registered",
+      });
+    }
+
+    //generate otp
+    var otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+    console.log("OTP generated : ", otp);
+
+    //check unique otp or not
+    let result = await OTP.findOne({ otp: otp });
+    //ye package hume jaruri nhi hai har baar unique otp de esliye aisa karna pad rha hai
+    while (result) {
+      otp = otpGenerator(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+      result = await OTP.findOne({ otp: otp });
+    }
+    const otpPayload = { email, otp }; // date not inserted because we already set it default
+
+    //create an entry for OTP
+    const otpBody = await OTP.create(otpPayload);
+    console.log(otpBody);
+
+    //return response successful
+    res.status(200).json({
+      success: true,
+      message: "OTP sent Successfully",
+      otp,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 //signup
+exports.signUp = async (req, res) => {
+  try {
+    //extract data from body
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      accountType,
+      contactNumber,
+      otp,
+    } = req.body;
+    //validate data
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !otp
+    ) {
+      return res.status(403).json({
+        success: false,
+        message: "All Fields are Required",
+      });
+    }
+    //check password and confirm password
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Password and ConfirmPassword value does not match, Please try again",
+      });
+    }
+    // finding user already existed or not
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User is Already Registered",
+      });
+    }
+    //find most recent OTP stored for the user
+    const recentOtp = await OTP.find({ email })
+      .sort({ createdAt: -1 })
+      .limit(1);
+    console.log(recentOtp);
+    //validate OTP
+    if (recentOtp.length == 0) {
+      //OTP not found
+      return res.status(400).json({
+        success: false,
+        message: "OTP Not Found",
+      });
+    } else if (otp !== recentOtp.otp) {
+      //Invalid OTP
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP",
+      });
+    }
+    //hash password
+    const hasedPassword = await bcrypt.hash(password, 10);
+    //entry create in db
+
+    const profileDetails = await Profile.create({
+      gender: null,
+      dateOfBirth: null,
+      about: null,
+      contactNumber: null,
+    });
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      hasedPassword,
+      password: hasedPassword,
+      contactNumber,
+      accountType,
+      additionalDetails: profileDetails._id,
+      image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName}${lastName}`,
+    });
+
+    //return response
+    return res.status(200).json({
+      success: true,
+      message: "User is registered Successfully",
+      user,
+    });
+  } catch (errror) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "User Cannot be registered. Please try again",
+      user,
+    });
+  }
+};
+
 //Login
+exports.login = async (req, res) => {
+  try {
+    //get data from body
+  } catch (error) {
+    console.log(error);
+    return res.status().json({
+      success: false,
+      message: "",
+    });
+  }
+};
 //changePassword
